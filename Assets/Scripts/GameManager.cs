2982f8cbaa63ccc04a5e9b9883c7c887995e8200
LiangcaiChen <chenl47@cardiff.ac.uk>
2016-12-22 20:48:35 +0000

    add path finding for enemy one

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+
+	public GameObject spawnPoint;
+	public GameObject[] enemies;
+	public int maxEnemiesOnScreen;
+	public int totalEnemies;
+	public int enemiesPerSpawn;
+
+	private int enemiesOnScreen = 0;
+
+	void Awake() {
+		if(instance == null) {
+			instance = this;
+		} else if (instance != this) {
+			Destroy(gameObject);
+		}
+
+		DontDestroyOnLoad(gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+		spawnEnemy();
+	}
+	
+	void spawnEnemy() {
+		if(enemiesPerSpawn > 0 && enemiesOnScreen < totalEnemies) {
+			for(int i = 0; i < enemiesPerSpawn; i++) {
+				if(enemiesOnScreen < maxEnemiesOnScreen) {
+					GameObject newEnemy = Instantiate(enemies[0]) as GameObject;
+					newEnemy.transform.position = spawnPoint.transform.position;
+					enemiesOnScreen += 1;
+				}
+			}
+		}
+	}
+
+	public void removeEnemyFromScreen() {
+		if(enemiesOnScreen > 0) {
+			enemiesOnScreen -= 1;
+		}
+	}
+
+}
